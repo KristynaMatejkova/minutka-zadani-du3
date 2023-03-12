83811491987de0889291fc8eb8ab6afb38c2e184
LAPTOP-6A2IC92J\rdhd1 <junova.kristyna@gmail.com>
2023-03-12 17:08:26 +0100

    d3

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('funguju!');
+/*nedokážu přijít na to, jak zařídit, aby když chci provést postupně obě zvonění, tak aby se u obou z nich budík rozhýbal
+tou CSS třídou...když nemám to myTimeout zakomentované, tak se provedou obě zvonění, ale jen u prvního z nich se budík rozhýbe*/
+
+const budik = document.querySelector('.alarm')
+
+const timeIsUp = () => {
+    budik.classList.add('alarm--ring')
+    const alarm = document.querySelector('audio')
+    alarm.play()
+}
+
+//let myTimeout=setTimeout(timeIsUp, 5000)
+
+let vstupUzivatel = prompt('Zadejte, kolik vteřin má uplynout než minutka začne zvonit:')
+let uzivatelTimeout = setTimeout(timeIsUp, Number(vstupUzivatel*1000))
+
+const stopMinutka = () => {
+    //clearTimeout(myTimeout)
+    clearTimeout(uzivatelTimeout)
+}
